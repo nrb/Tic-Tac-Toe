0c5873748a4000c396ec9d02df99f759da2855e2
Nolan Brubaker <palendae@gmail.com>
2011-04-13 14:03:02 -0500

    Corrected typos and bugs in the move method.
    The += operator wasn't working for appending moves to a list, so changed
    it to .append.
    
    Deleting items from the magic square list was shifting the indices of
    remaining items.  Changed it to mark played spots as 'None'.

--- ttt.py
+++ ttt.py
@@ -1,16 +1,18 @@
 #!/bin/env python
 
 
-import intertools, collections
+import itertools, collections
 
 class Player(object):
     def __init__(self, game, symbol):
-        self.moves, self.game self.symbol = [], game, symbol
+        self.moves, self.game, self.symbol = [], game, symbol
 
     def move(self, idx):
         """Mark a given index as played and check for a win."""
-        self.moves += self.game.magic_square[idx]
-        del self.game.magic_square[idx]
+        self.moves.append(self.game.magic_square[idx])
+
+        # Mark the space as None, cause deleting it shifts the indices.
+        self.game.magic_square[idx] = None
         if len(self.moves) >= 3:
             checked_combos = [self.game.check_for_win(combo) for combo in itertools.combinations(self.moves, 3)]
 
@@ -39,5 +41,14 @@ class Game(object):
 if __name__ == '__main__':
     # Some testing of the functionality.
 
-    player = Player()
     game = Game()
+    player1 = Player(game, 'X')
+    player2 = Player(game, 'O')
+
+    player1.move(0)
+    assert 8 not in game.magic_square
+    assert 8 in player1.moves
+
+    player2.move(4)
+    assert 5 not in game.magic_square
+    assert 5 in player2.moves
