a42d8ac4d0f4f158da354e1c8f203b93c8913722
Nolan Brubaker <palendae@gmail.com>
2011-04-13 13:51:28 -0500

    Fleshed out the move and check_for_win methods.
    Added combinations for user moves, as well as logic to do the validation
    of the comobo as a winner.

--- ttt.py
+++ ttt.py
@@ -4,24 +4,36 @@
 import intertools, collections
 
 class Player(object):
-    def __init__(self, symbol):
-        self.moves, self.symbol = [], symbol
+    def __init__(self, game, symbol):
+        self.moves, self.game self.symbol = [], game, symbol
 
     def move(self, idx):
         """Mark a given index as played and check for a win."""
-        pass
+        self.moves += self.game.magic_square[idx]
+        del self.game.magic_square[idx]
+        if len(self.moves) >= 3:
+            checked_combos = [self.game.check_for_win(combo) for combo in itertools.combinations(self.moves, 3)]
+
+            if True in checked_combos:
+                self.game.winner = self
 
 class Game(object):
-    magic_square = [ 8, 1, 6,
-                     3, 5, 7,
-                     4, 9, 2]
+    def __init__(self):
+        self.magic_square = [ 8, 1, 6,
+                              3, 5, 7,
+                              4, 9, 2]
+
+        self.winner = None
 
     def check_for_win(self, combo):
         """
         Check the combinations created by the player's move
         and see if they got a win.
         """
-        pass
+        if sum(combo) == 15:
+            return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
